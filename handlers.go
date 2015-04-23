7417f0162afd35ed0d24dea4e2d570b71f6d5cdb
Anthony Alaribe <anthonyalaribe@ymail.com>
2015-04-23 06:17:43 +0000

    dsaf

--- handlers.go
+++ handlers.go
@@ -104,7 +104,7 @@ func SingleHandlerWeb(w http.ResponseWriter, r *http.Request) {
 			Id:      session.Values["Id"].(string),
 		}
 
-		err := AddReview(&rr)
+		err = AddReview(&rr)
 		checkFmt(err)
 
 	}
